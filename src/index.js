22ae69eb290225ba5f83ab1224018185731ce547
liujiaxu <1668329679@qq.com>
2019-01-07 14:07:56 +0800

    1.1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "view/app";
+
+import "common/css/bootstrap.min.css";
+import "common/css/reset.css";
+import "common/css/common.css";
+import "common/css/style.css";
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
